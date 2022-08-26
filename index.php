749db4cf3a06f9699d6a0d3e81492b38bf65bf07
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-08-26 16:42:55 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
+echo "<!DOCTYPE HTML>";
+echo "<html>";
+echo "<head>";
+echo "<link rel='stylesheet' href='https://lucky2245.github.io/website/index.css'/>";
+echo "</head>";
+echo "<body>";
+echo "<h1 class='titleheader'>Php Site</h1>";
 ?>
